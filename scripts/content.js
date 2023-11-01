21599fb48c9cd25b31990014bb3662246bcbd954
Shihchi Hsu <schi.hsu@gmail.com>
2023-11-01 13:33:17 +0800

    feat: update button to link list page

--- scripts/content.js
+++ scripts/content.js
@@ -1,39 +1,80 @@
-const repoList = [
-  ...document.querySelectorAll(
-    '#user-starred-repos div.starred details-menu.SelectMenu[role="menu"]'
-  ),
-];
+function getSelectMenuList() {
+  return [
+    ...document.querySelectorAll(
+      '#user-starred-repos div.starred details-menu.SelectMenu[role="menu"]'
+    ),
+  ]
+}
 
+async function getTags(detailsMenu) {
+  const link = detailsMenu.attributes.getNamedItem("src")?.value || ''
+
+  const response = await fetch(link)
+  const htmlString = await response.text()
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlString, 'text/html');
+  const inputs = [...doc.querySelectorAll('input:checked')];
+
+  const tags = inputs
+    .map((input) => input.parentNode?.querySelector("span")?.textContent?.trim())
+
+  const batchUrl = doc.querySelector("div")?.attributes.getNamedItem("data-batch-update-url")?.value
+  let userId = ""
+  if (typeof batchUrl === 'string') {
+    userId = batchUrl.split("/")[2]
+  }
+
+  return { tags, userId }
+}
+
+function genSpan(tag) {
+  const span = document.createElement('span');
+  span.textContent = tag || ""
+  span.classList.add("Label", "Label--success", "Label--inline", "px-2", "mr-2")
+  span.style.lineHeight = "22px"
+  return span
+}
+
+function genButtom(tag, userId) {
+  const a = document.createElement("a");
+
+  a.href = `/stars/${userId}/lists/${tag.replace(/ /g, "-")}`;
+  a.classList.add("Button", "Button--secondary", "Button--small", "mr-1");
+
+  const innerSpan1 = document.createElement("span");
+  innerSpan1.classList.add("Button-content");
+
+  const innerSpan2 = document.createElement("span");
+  innerSpan2.classList.add("Button-label");
+
+  const innerSpan3 = document.createElement("span");
+  innerSpan3.classList.add("v-align-middle");
+  innerSpan3.textContent = tag;
+
+  innerSpan2.appendChild(innerSpan3);
+  innerSpan1.appendChild(innerSpan2);
+  a.appendChild(innerSpan1);
+
+  return a
+}
+
+const repoList = getSelectMenuList()
 console.log(`repoList: ${repoList.length}`)
 
 for (const repo of repoList) {
-  const link = repo.attributes.getNamedItem("src")?.value || ''
-
-  fetch(link)
-    .then(function (response) {
-      return response.text();
+  getTags(repo)
+    .then(({ tags, userId }) => {
+      return tags.map((tag) => genButtom(tag, userId))
     })
-    .then(function (htmlString) {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(htmlString, 'text/html');
-      const inputs = [...doc.querySelectorAll('input:checked')];
-
-      const tags = inputs
-        .map((input) => input.nextElementSibling?.textContent?.trim())
-        .map((tag) => {
-          const span = document.createElement('span');
-          span.textContent = tag || ""
-          span.classList.add("Label", "Label--success", "Label--inline", "px-2", "mr-2")
-          span.style.lineHeight = "22px"
-          return span
-        })
-
-      const starBtnGroup = repo.parentElement.parentElement
+    .then((spans) => {
+      const starBtnGroup = repo.parentElement?.parentElement
+      if (starBtnGroup === null || starBtnGroup === undefined) {
+        return
+      }
       const firstChild = starBtnGroup.firstElementChild;
-      tags.forEach(span => {
+      spans.forEach(span => {
         starBtnGroup.insertBefore(span, firstChild);
       });
-
-    });
-
+    })
 }
